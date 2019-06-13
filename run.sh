d801a5ff790d1b8c0a7cdcd2d5648a3f4c508500
andriish <andriish@users.noreply.github.com>
2019-06-13 12:51:38 +0200

    OK

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-for a in $(ls -1 cards/E0*); do
+for a in $(ls -1 cards/E0*input); do
 ./eerad3 -i $a -n 0 &
 done
 
 
 #!/bin/bash
-for a in $(ls -1 cards/Et*); do
+for a in $(ls -1 cards/Et*input); do
 ./eerad3 -i $a -n 1 &
 ./eerad3 -i $a -n 2 &
 ./eerad3 -i $a -n 3 &
@@ -13,10 +13,8 @@ done
 
 
 wait 
-
-
-./eerad3_combine
-
-
+for a in $(ls -1 cards/Et*combine); do
+./eerad3_combine -i $a 
+done
 
 ./eerad3_dist
