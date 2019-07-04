64d5d85bdf30e2e96728d76841b6d740c3922a1f
andriish <andriish@users.noreply.github.com>
2019-07-04 13:17:54 +0200

    OK

--- grid/EERAD3-arc-cancel.sh
+++ grid/EERAD3-arc-cancel.sh
@@ -8,32 +8,6 @@ arckill -j jobs.xml $b
 done
 
 
-exit
-   56 files in directory ./JRTMC000/herwig71openloopsmergeddipolec/130
 
-  148 files in directory ./JRTMC000/sherpaopenloopsc/130
-  304 files in directory ./JRTMC000/sherpaopenloopsc/35
-
-
-   16 files in directory ./JRTMC000/sherpaopenloopsc/133
-   10 files in directory ./JRTMC000/sherpaopenloopsc/161
-    6 files in directory ./JRTMC000/sherpaopenloopsc/183
-    8 files in directory ./JRTMC000/sherpaopenloopsc/172
-    1 files in directory ./JRTMC000/sherpaopenloopsc/136
-
-
-  225 files in directory ./JRTMC000/sherpaopenloopsl/172
-
-
-    1 files in directory ./JRTMC000/sherpaopenloopsl/194
-    1 files in directory ./JRTMC000/sherpaopenloopsl/189
-    1 files in directory ./JRTMC000/sherpaopenloopsl/183
-    1 files in directory ./JRTMC000/sherpaopenloopsl/200
-    1 files in directory ./JRTMC000/sherpaopenloopsl/196
-    1 files in directory ./JRTMC000/sherpaopenloopsl/192
-    1 files in directory ./JRTMC000/sherpaopenloopsl/205
-    1 files in directory ./JRTMC000/sherpaopenloopsl/206
-    1 files in directory ./JRTMC000/sherpaopenloopsl/202
-    1 files in directory ./JRTMC000/sherpaopenloopsl/207
 
 
